a262724982590af80210384a6e91db195a238871
WayneKuo <aasdfangela@gmail.com>
2017-12-19 11:48:42 +0800

    Spinner

--- app/src/main/java/com/wayne/atm/UserInfoActivity.java
+++ app/src/main/java/com/wayne/atm/UserInfoActivity.java
@@ -3,18 +3,36 @@ package com.wayne.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private static final String TAG = Ch8Activity.class.getSimpleName();
     private EditText edName;
     private EditText edPhone;
+    private Spinner ages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+        ages = (Spinner) findViewById(R.id.ages);
+   /*     ArrayList<String> data = new ArrayList<>();
+        for (int i = 15; i <= 40; i++) {
+            data.add(i + "");
+        }*/
+        String[] data = getResources().getStringArray(R.array.ages);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, ages.setAdapter(adapter);
+        ages.setAdapter(adapter);
+
         edName = (EditText) findViewById(R.id.ed_name);
         edPhone = (EditText) findViewById(R.id.ed_phone);
         setContentView(R.layout.activity_user_info);
@@ -28,6 +46,8 @@ public class UserInfoActivity extends AppCompatActivity {
 
     public void Ok (View v){
     //    Button Ok = (Button) findViewById(R.id.b_ok);
+        Log.d(TAG,"ok:" + ages.getSelectedItem().toString());
+        int age = Integer.parseInt(ages.getSelectedItem().toString());
         String name = edName.getText().toString();
         String phone = edPhone.getText().toString();
         getIntent().putExtra("EXTRA_NAME", name);
@@ -37,4 +57,5 @@ public class UserInfoActivity extends AppCompatActivity {
         finish();
 
     }
+
 }
